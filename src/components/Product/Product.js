e7eab24e633a87286262a1ef798afb9bc05157e3
Kristou <kristiyan.svetoslavov.petkov@gmail.com>
2021-09-21 14:14:33 +0200

    finished Home page

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,23 +1,25 @@
 import React from "react";
 import "./Product.css";
 
-function Product() {
+function Product({ id, title, image, price, raiting }) {
   return (
     <div className="product">
       <div className="product__info">
-        <p>The lean startup</p>
+        <p>{title}</p>
         <p className="product__price">
           <small>$</small>
-          <strong>30</strong>
+          <strong>{price}</strong>
         </p>
+        {/*Display stars */}
         <div className="product__raiting">
-          <p>⭐</p>
+          {Array(raiting)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SY291_BO1,204,203,200_QL40_ML2_.jpg"
-        alt="lean startup"
-      />
+      <img src={image} alt="lean startup" />
       <button>Add to basket</button>
     </div>
   );
